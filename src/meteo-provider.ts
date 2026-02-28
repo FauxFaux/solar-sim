1acf20359696647e3350bf31b75c59c13c1d89cf
Chris West (Faux) <git@goeswhere.com>
2026-02-28 11:10:27 +0000

    fix: hack around vite/node disagreeing on attributse
    https://github.com/vitejs/vite/issues/18582

--- src/meteo-provider.ts
+++ src/meteo-provider.ts
@@ -2,9 +2,11 @@ import { range } from './granite/numbers.ts';
 import { deltaDecode, MCS_ZONE_CENTRES } from './system/mcs-meta.ts';
 import { createContext } from 'preact';
 import { sleep, type State, tryTo } from './ts.ts';
-const meteo6 = await import('./assets/meteo-6.json', {
-  with: { type: 'json' },
-});
+const meteo6 = import.meta.env?.MODE
+  ? await import('./assets/meteo-6.json')
+  : await import('./assets/meteo-6.json', {
+      with: { type: 'json' },
+    });
 
 export interface Meteo {
   temp: number[];
@@ -40,9 +42,17 @@ export async function loadMeteosRaw() {
   await sleep(10);
   return await Promise.all(
     range(MCS_ZONE_CENTRES.length).map(async (zone) => {
-      const { default: data } = await import(`./assets/meteo-${zone}.json`, {
-        with: { type: 'json' },
-      });
+      let data: unknown;
+      // partial workaround for https://github.com/vitejs/vite/issues/18582
+      if (import.meta.env?.MODE) {
+        data = (await import(`./assets/meteo-${zone}.json`)).default;
+      } else {
+        data = (
+          await import(/* @vite-ignore */ `./assets/meteo-${zone}.json`, {
+            with: { type: 'json' },
+          })
+        ).default;
+      }
       return decodeMeteo(data as EncodedMeteo);
     }),
   );
